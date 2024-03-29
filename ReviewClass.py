09f5d04c9969cf8cba1ec1b610f1dca2e40734a8
matthewstevens <mstevens@ithaca.edu>
2024-03-28 23:09:07 -0400

    Set up review class

---
+++ ReviewClass.py
@@ -0,0 +1,55 @@
+'''
+    Class Outline: Matt
+'''
+
+class Review:
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    '''Returns the ID of the review'''
+    def getID(self):
+        pass
+
+    '''Returns the user who wrote the review'''
+    def getUser(self):
+        pass
+
+    '''Sets the user for a new review'''
+    def setUser(self, User):
+        pass
+
+    '''Returns the book that the review is about'''
+    def getBook(self):
+        pass
+
+    '''Ties the review to a book'''
+    def setBook(self, Book):
+        pass
+
+    '''Returns the date the review was written'''
+    def getDate(self):
+        pass
+
+    '''Sets the date for a new review'''
+    def setDate(self, Date):
+        pass
+
+    '''Gets the rating from a review'''
+    def getRate(self):
+        pass
+
+    '''Allows teh user to rate a book with the review'''
+    def setRate(self, rating):
+        pass
+
+    '''Returns the contents of the review'''
+    def getContent(self):
+        pass
+
+    '''Allows teh user to write a review paragraph'''
+    def setContent(self, content):
+        pass
