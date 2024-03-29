7fda07b54c69a59b730599c996c56036ceb9b757
matthewstevens <mstevens@ithaca.edu>
2024-03-28 23:13:12 -0400

    Set up review class Tests

---
+++ ReviewClassTest.py
@@ -0,0 +1,52 @@
+'''
+    Class outline: Matt
+'''
+
+
+import pytest
+import ReviewClass
+
+
+'''Returns the ID of the review'''
+def getIDTest():
+    pass
+
+'''Returns the user who wrote the review'''
+def getUserTest():
+    pass
+
+'''Sets the user for a new review'''
+def setUserTest():
+    pass
+
+'''Returns the book that the review is about'''
+def getBookTest():
+    pass
+
+'''Ties the review to a book'''
+def setBookTest():
+    pass
+
+'''Returns the date the review was written'''
+def getDateTest():
+    pass
+
+'''Sets the date for a new review'''
+def setDateTest():
+    pass
+
+'''Gets the rating from a review'''
+def getRateTest():
+    pass
+
+'''Allows teh user to rate a book with the review'''
+def setRateTest():
+    pass
+
+'''Returns the contents of the review'''
+def getContentTest():
+    pass
+
+'''Allows teh user to write a review paragraph'''
+def setContentTest():
+    pass
