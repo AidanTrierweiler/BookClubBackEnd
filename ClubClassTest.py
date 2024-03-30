4bdf610e0bc437b777c7c0f4d165ee66a988e430
cprochilo <cprochilo@ithaca.edu>
2024-03-29 20:00:11 -0400

    Wrote headers for ClubClassTest.py

---
+++ ClubClassTest.py
@@ -0,0 +1,111 @@
+'''
+Outline: Cole
+'''
+
+'''
+Assures the club object is created with all the correct attributes.
+'''
+def constructorTest():
+    pass
+
+'''
+Assures that the repr method returns the correct string
+'''
+def reprTest():
+    pass
+
+'''
+Assures the correct ID is returned
+'''
+def getId():
+    pass
+
+'''
+Assures the correct name is returned
+'''
+def getName():
+    pass
+
+'''
+Assures that when the name attribute is altered the string is updated correctly
+'''
+def setName():
+    pass
+
+'''
+Assures that the correct user object is returned when the director is asked for
+'''
+def getDirector():
+    pass
+
+'''
+Assures that the director attribute is updated correctly
+'''
+def setDirector():
+    pass
+
+'''
+Assures that when the monthly host is asked for the correct user object is returned
+'''
+def getMonthlyHost():
+    pass
+
+'''
+Assures that the monthly host attribute is updated correctly
+'''
+def setMonthlyHost():
+    pass
+
+'''
+Assures that the correct list of users is returned
+'''
+def getMembers():
+    pass
+
+'''
+Assures that the correct user object is returned when asking for a specific member of the club
+'''
+def getMember():
+    pass
+
+'''
+Assures that the user list attribute is updated ocrrectly when a member is added to the club
+'''
+def addMember():
+    pass
+
+'''
+Assures that the correct list of books is returned
+'''
+def getBooks():
+    pass
+
+'''
+Assures the correct book object is returned when a specific one is asked for
+'''
+def getBook():
+    pass
+
+'''
+Assures that the books list is updated correctly
+'''
+def addBook():
+    pass
+
+'''
+Assures that the correct list of locations is returned
+'''
+def getLocations():
+    pass
+
+'''
+Assures that the meeting location attribute is updated correctly
+'''
+def setMeetingLocation():
+    pass
+
+'''
+Assures that the correct meeting location is returned
+'''
+def getMeetingLocation():
+    pass
