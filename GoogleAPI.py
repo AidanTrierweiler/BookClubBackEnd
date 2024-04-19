3ff0ddcccf68cc5a1127ea2a613c289c7759d44f
kaidenjones <zjones@ithaca.edu>
2024-04-19 10:42:29 -0400

    updated README.md

--- GoogleAPI.py
+++
@@ -1,7 +0,0 @@
-
-'''API Key: AIzaSyCeqovBPCwT5vV-zNGEhi4cJbi2iT3ObZs'''
-
-import requests
-
-response = requests.get("https://www.googleapis.com/books/v1/volumes?q=search+terms")
-print(response.json())
