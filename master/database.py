7a0ecd35d97c6579d23adcba435c08c84e89628d
Aidan Trierweiler <atrierweiler@ithaca.edu>
2024-04-19 10:13:01 -0400

    database file

--- master/database.py
+++ master/database.py
@@ -6,6 +6,6 @@ URL_DATABASE = 'mysql+pymysql://root:DougProject3!@localhost:3306/bookclubdb'
 
 engine = create_engine(URL_DATABASE)
 
-SessionLocal = sessionmaker(autocmmit=False, autoflash=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflash=False, bind=engine)
 
 Base = declarative_base
